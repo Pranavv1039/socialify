4094660618561e85f19ecc158185bcc2b021539f
Shiva-shankar-mahadevu <mahadevushiva58@gmail.com>
2025-08-09 22:19:48 +0530

    scripts added

--- Socialify/client/src/config.js
+++ Socialify/client/src/config.js
@@ -1,4 +1,6 @@
-let BASE_URL = "https://socialify-ch3i.onrender.com/";
+let BASE_URL = "http://<your-ec2-public-ip>:5000/";
+
+// Optional: fallback to localhost in development only
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
