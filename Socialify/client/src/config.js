d39d4feafc0aae3630a48768a12be4886a81095b
Shiva-shankar-mahadevu <mahadevushiva58@gmail.com>
2025-09-13 20:47:12 +0530

    final changes-1

--- Socialify/client/src/config.js
+++ Socialify/client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:4000/";
 
 // Optional: fallback to localhost in development only
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
